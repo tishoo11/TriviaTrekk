a2202ab9b4cf14b272a397e325394f0ca5644d5b
Kebabche11 <tishoyanev08@gmail.com>
2025-07-12 18:18:26 +0300

    Program 1st half

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,13 @@ namespace TriviaTrekk
     {
         static void Main(string[] args)
         {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "questions.csv");
+            QuestionRepository repo = new QuestionRepository(filePath);
 
+            Console.Write("Име на играч 1: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Име на играч 2: ");
+            string name2 = Console.ReadLine();
         }
     }
 }
