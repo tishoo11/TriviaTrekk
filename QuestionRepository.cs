53d868d0ba24f887b21ff83143f9d28f06b761b5
Kebabche11 <tishoyanev08@gmail.com>
2025-07-12 17:35:11 +0300

    Added code to QuestionRepository

--- QuestionRepository.cs
+++ QuestionRepository.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace TriviaTrekk
 {
-    internal class QuestionRepository
+    public class QuestionRepository
     {
+        List<Questions> questions = new List<Questions>();
+        Random random = new Random();
+
+        public QuestionRepository(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                var parts = line.Split(';');
+                if (parts.Length < 8) continue;
+
+                questions.Add(new Questions
+                {
+                    ID = parts[0],
+                    Text = parts[1],
+                    Answers = new string[] { parts[2], parts[3], parts[4], parts[5] },
+                    CorrectAnswer = int.Parse(parts[6]),
+                    Crux = int.Parse(parts[7])
+                });
+            }
+        }
     }
-}
