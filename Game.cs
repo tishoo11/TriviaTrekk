8dc2c81309fa8635c51d3479d01fbc1a1ab1e09b
plamenivn <palmenivanov768@gmail.com>
2025-07-12 18:21:11 +0300

    add class Game.cs add commit 1

---
+++ Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaTrekk
+{
+    public class Game
+    {
+        QuestionRepository repo;
+        Player[] players;
+        int currentPlayerIndex;
+        Random random = new Random();
+        int Finish = 50;
+
+        public Game(QuestionRepository repository, Player p1, Player p2)
+        {
+            repo = repository;
+            players = new Player[] { p1, p2 };
+            currentPlayerIndex = 0;
+        }
+    }
+
+}
