7ce0423e5ff144cc75440154715da753e9bcbc61
Kebabche11 <tishoyanev08@gmail.com>
2025-07-12 18:18:44 +0300

    Program 2nd half

--- Program.cs
+++ Program.cs
@@ -18,6 +18,15 @@ namespace TriviaTrekk
             string name1 = Console.ReadLine();
             Console.Write("Име на играч 2: ");
             string name2 = Console.ReadLine();
+
+            Player p1 = new Player(name1);
+            Player p2 = new Player(name2);
+
+            Game game = new Game(repo, p1, p2);
+            game.PlayInteractive();
+
+            Console.WriteLine("Играта приключи. Натиснете клавиш за изход.");
+            Console.ReadKey();
         }
     }
 }
